0deeaea7917d8de4a38717060280be334d0f0146
My Name <amine@post.com>
2022-11-20 21:31:33 +0100

    Config database connection with typeorm, add user module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import * as dotenv from 'dotenv';
+// import { LoggerModule } from './logger/logger.module';
+import { UserHttpModule } from './user/user-http.module';
 
-import { DatabaseModule } from './database/database.module';
-
+dotenv.config();
 
 @Module({
   imports: [
-    DatabaseModule
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+    // LoggerModule,
+    UserModule,
+    UserHttpModule,
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {}
+}
